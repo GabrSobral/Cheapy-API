28408acc97c78dd05180771a55e600885dea31f3
Sobraloser <Asgoth55@gmail.com>
2021-12-22 13:44:24 -0300

    feat: add User Model

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace Cheapy_API.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        
+        [JsonIgnore]
+        public string Password { get; set; }
+    }
+}
