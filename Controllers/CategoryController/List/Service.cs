d17e2aaf930c5b3aa34c961b99f387e69d556bcf
Sobraloser <Asgoth55@gmail.com>
2021-12-24 11:11:03 -0300

    feat: add CategoryController list method

---
+++ Controllers/CategoryController/List/Service.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cheapy_API.Models;
+using Cheapy_API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cheapy_API.Controllers.CategoryController.List
+{
+    public class Service
+    {
+        public async Task<List<Category>> Execute(AppDbContext context)
+        {
+            var categories = await context.Categories
+                .AsNoTracking()
+                .ToListAsync();
+                
+            return categories;
+        }
+    }
+}
