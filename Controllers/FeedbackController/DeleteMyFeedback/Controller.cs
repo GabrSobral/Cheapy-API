dbaff46158ca58440ee50dcd84b8b05810d4ce79
Sobraloser <Asgoth55@gmail.com>
2021-12-27 17:08:53 -0300

    feat: add FeedbackController DeleteMyFeedback

---
+++ Controllers/FeedbackController/DeleteMyFeedback/Controller.cs
@@ -0,0 +1,31 @@
+using System;
+using Cheapy_API.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Cheapy_API.Services;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Cheapy_API.Controllers.FeedbackController.DeleteMyFeedback
+{
+    [ApiController]
+    [Route("v1")]
+    public class Controller : BaseController
+    {
+        [Authorize]
+        [HttpDelete("feedbacks/{id}")]
+        public async Task<IActionResult> Handle(
+            [FromServices] AppDbContext context,
+            [FromRoute] Guid id)
+        {
+            try
+            {
+                await new Service().Execute(context, productId: id, userId: GetUserId());
+                return Ok();
+            }
+            catch(Exception error)
+            {
+                return new ErrorCatcher(error).Return();
+            }
+        }
+    }
+}
---
+++ Controllers/FeedbackController/DeleteMyFeedback/Service.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Cheapy_API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cheapy_API.Controllers.FeedbackController.DeleteMyFeedback
+{
+    public class Service
+    {
+        public async Task Execute(AppDbContext context, Guid productId, Guid userId)
+        {
+            var product = await context.Products
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == productId);
+
+            if(product == null)
+                throw new Exception("Product not found status:404");
+
+            var feedback = await context.Feedbacks
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => ((x.ProductId == productId) && (x.UserId == userId)));
+            
+            if(feedback == null)
+                throw new Exception("You don't have feedbacks of this product yet status:400");
+            
+            context.Feedbacks.Remove(feedback);
+            await context.SaveChangesAsync();
+        }
+    }
+}
