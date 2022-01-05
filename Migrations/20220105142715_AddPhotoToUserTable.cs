f6d50bbda9aa7a821322ebcdb64cc7a2016ad88f
Sobraloser <Asgoth55@gmail.com>
2022-01-05 11:48:39 -0300

    feat: add photo to User model

---
+++ Migrations/20220105142715_AddPhotoToUserTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cheapy_API.Migrations
+{
+    public partial class AddPhotoToUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Users");
+        }
+    }
+}
