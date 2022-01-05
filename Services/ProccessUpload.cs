91ee92a1195c13bb3931a4e7a4c147278b1cf031
Sobraloser <Asgoth55@gmail.com>
2022-01-05 11:48:14 -0300

    feat: add processUpload class

--- Services/ProccessUpload.cs
+++ Services/ProccessUpload.cs
@@ -8,22 +8,30 @@ namespace Cheapy_API.Services
     public class ProccessUpload
     {
         private readonly IWebHostEnvironment _webHostEnvironment;  
+        private readonly string UploadFolder;
+
         public ProccessUpload(IWebHostEnvironment webHostEnvironment)
         {
             _webHostEnvironment = webHostEnvironment;
+            UploadFolder = Path.Combine(_webHostEnvironment.WebRootPath, "Uploads");
         }
 
         public string Upload(IFormFile file)
         {
-            string uploadFolder = Path.Combine(_webHostEnvironment.WebRootPath, "Uploads");
             string uniqueFileName = $"{Guid.NewGuid().ToString()}_{file.FileName}";
-            string FilePath = Path.Combine(uploadFolder, uniqueFileName);
+            string FilePath = Path.Combine(UploadFolder, uniqueFileName);
 
             using (var fileStream = new FileStream(FilePath, FileMode.Create))
             {
                 file.CopyTo(fileStream);
             }
             return uniqueFileName;
+        }
+
+        public void DeleteFile(string fileName)
+        {
+            string FilePath = Path.Combine(UploadFolder, fileName);
+            File.Delete(FilePath);
         }
     }
 }
